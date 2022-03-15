b7f8ab4dccd72ebbd74c6172964fe2fb1dc871f8
Joelimgu <joelimgu@gmail.com>
2022-03-15 21:30:53 +0100

    herkulex files

---
+++ herkulex/src/main.rs
@@ -0,0 +1,16 @@
+
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use stm32f1xx_hal::prelude::*;
+
+
+#[entry]
+fn main() -> ! {
+    loop {
+
+    }
+}
