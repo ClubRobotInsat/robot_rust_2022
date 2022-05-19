802bfb30183def904cbffb3ff563770701412a33
Ronan <ronan.bonnet42@wanadoo.fr>
2022-05-19 17:52:27 +0200

    Removed arrays because it was useless and it did not work.
    Removed arrays to store groups and servos because it did not work. The array of group is useless. However, the array of servos is important to consider moving a group of servos by giving an equation for example.

--- asm/herkulex/src/motors/mod.rs
+++ asm/herkulex/src/motors/mod.rs
@@ -1,5 +1,7 @@
 use crate::motors::communication::HerkulexCommunication;
+use core::borrow::Borrow;
 use core::cell::RefCell;
+use cortex_m_semihosting::hprintln;
 
 use crate::motors::motor_group::MotorGroup;
 
@@ -11,42 +13,19 @@ const GROUP_SIZE: usize = 20;
 
 // const INIT_MOTORGROUP : Option<RefCell<MotorGroup>> = None;
 
-pub struct Motors<'a, Comm: HerkulexCommunication> {
+pub struct Motors<Comm: HerkulexCommunication> {
     communication: RefCell<Comm>,
-    groups: [RefCell<Option<MotorGroup<'a, Comm>>>; GROUP_SIZE],
 }
 
-impl<'a, Comm: HerkulexCommunication> Motors<'a, Comm> {
-    pub fn new(comm: Comm) -> Motors<'a, Comm> {
+impl<'a, Comm: HerkulexCommunication> Motors<Comm> {
+    pub fn new(comm: Comm) -> Motors<Comm> {
         Motors {
             communication: RefCell::new(comm),
-            groups: Default::default(), // groups: [INIT_MOTORGROUP; GROUP_SIZE]
         }
     }
 
-    pub fn new_group(&'a self) -> usize {
-        let i = self.number_of_groups();
-        self.groups[i]
-            .borrow_mut()
-            .get_or_insert(MotorGroup::new(&self.communication));
-        i
-
-        //= Some(RefCell::new(group)); // Add the new group to the array
-        //let q = &self.groups.borrow_mut()[i];
-        // Returns a reference of the group
-        //q.borrow_mut().unwrap()
-    }
-
-    pub fn get_group(&self, id: usize) -> &RefCell<Option<MotorGroup<'a, Comm>>> {
-        self.groups.get(id).unwrap()
-    }
-
-    pub fn number_of_groups(&self) -> usize {
-        let mut i = 0;
-        while self.groups[i].borrow().is_none() {
-            i += 1;
-        }
-
-        i
+    pub fn new_group(&self) -> MotorGroup<Comm> {
+        hprintln!("Motors::new_group");
+        MotorGroup::new(&self.communication)
     }
 }
--- asm/herkulex/src/motors/motor_group/mod.rs
+++ asm/herkulex/src/motors/motor_group/mod.rs
@@ -9,30 +9,18 @@ const GROUP_SIZE: usize = 20;
 
 pub struct MotorGroup<'a, Comm: HerkulexCommunication> {
     communication: &'a RefCell<Comm>,
-    motors: [RefCell<Option<Motor<'a, Comm>>>; GROUP_SIZE],
+    //motors: [RefCell<Option<Motor<'a, Comm>>>; GROUP_SIZE],
 }
 
 impl<'a, Comm: HerkulexCommunication> MotorGroup<'a, Comm> {
     pub fn new(comm: &'a RefCell<Comm>) -> MotorGroup<'a, Comm> {
         MotorGroup {
             communication: (comm),
-            motors: Default::default(), // motors: [INIT_MOTOR; GROUP_SIZE]
+            //motors: Default::default(), // motors: [INIT_MOTOR; GROUP_SIZE]
         }
     }
 
-    pub fn new_motor(&self, id: u8) -> &RefCell<Option<Motor<'a, Comm>>> {
-        let mut i = 0;
-
-        // Iteration throught groups and stop on the first empty cell to create a new motor
-        while self.motors[i].borrow().is_none() {
-            i += 1;
-        }
-
-        let motor = Motor::new(id, &self.communication);
-
-        // RefCell is to share memory
-        self.motors[i].borrow_mut().get_or_insert(motor); // Add the new group to the array
-                                                          //self.motors[i].as_ref().unwrap().borrow_mut()   // Returns a reference of the group
-        self.motors.get(i).unwrap()
+    pub fn new_motor(&self, id: u8) -> Motor<'a, Comm> {
+        Motor::new(id, &self.communication)
     }
 }
